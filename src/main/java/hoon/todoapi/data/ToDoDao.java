15e694180c5156687e962c801167ad1bbe3205f0
Hoon Kim <hoon0661@gmail.com>
2021-04-23 21:29:58 -0400

    first commit

---
+++ src/main/java/hoon/todoapi/data/ToDoDao.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hoon.todoapi.data;
+
+import hoon.todoapi.models.ToDo;
+import java.util.List;
+
+/**
+ *
+ * @author hoon0
+ */
+public interface ToDoDao {
+    ToDo add(ToDo todo);
+
+    List<ToDo> getAll();
+
+    ToDo findById(int id);
+
+    // true if item exists and is updated
+    boolean update(ToDo todo);
+
+    // true if item exists and is deleted
+    boolean deleteById(int id);
+}
